f9529ff811fa87510641094601aecad50d2b1ada
Magellanic <magellanicdev@gmail.com>
2018-08-23 14:58:28 +0100

    Audio tutorial

---
+++ TutorialSource/Audio.cs
@@ -0,0 +1,100 @@
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace MyAwesomeGame
+{
+	/// <summary>
+	/// This is the main type for your game.
+	/// </summary>
+	public class Game1 : Game
+	{
+		GraphicsDeviceManager graphics;
+		SpriteBatch spriteBatch;
+		Texture2D boxman;
+		Vector2 boxmanPos = new Vector2(200,200);
+		SoundEffect sfx;
+		SoundEffectInstance sfxInstance;
+
+		public Game1()
+		{
+			graphics = new GraphicsDeviceManager(this);
+			Content.RootDirectory = "Content";
+		}
+
+		/// <summary>
+		/// Allows the game to perform any initialization it needs to before starting to run.
+		/// This is where it can query for any required services and load any non-graphic
+		/// related content.  Calling base.Initialize will enumerate through any components
+		/// and initialize them as well.
+		/// </summary>
+		protected override void Initialize()
+		{
+			// TODO: Add your initialization logic here
+
+			base.Initialize();
+		}
+
+		/// <summary>
+		/// LoadContent will be called once per game and is the place to load
+		/// all of your content.
+		/// </summary>
+		protected override void LoadContent()
+		{
+			// Create a new SpriteBatch, which can be used to draw textures.
+			spriteBatch = new SpriteBatch(GraphicsDevice);
+			boxman = Content.Load<Texture2D>("boxman");
+			sfx = Content.Load<SoundEffect>("sfx_voice_00");
+			sfxInstance = sfx.CreateInstance();
+		}
+
+		/// <summary>
+		/// UnloadContent will be called once per game and is the place to unload
+		/// game-specific content.
+		/// </summary>
+		protected override void UnloadContent()
+		{
+			boxman.Dispose();
+		}
+
+		/// <summary>
+		/// Allows the game to run logic such as updating the world,
+		/// checking for collisions, gathering input, and playing audio.
+		/// </summary>
+		/// <param name="gameTime">Provides a snapshot of timing values.</param>
+		protected override void Update(GameTime gameTime)
+		{
+			float dt = (float)gameTime.ElapsedGameTime.TotalSeconds;
+
+			if(Keyboard.GetState().IsKeyDown(Keys.Up))
+				boxmanPos.Y -= 100.0f * dt;
+			if(Keyboard.GetState().IsKeyDown(Keys.Down))
+				boxmanPos.Y += 100.0f * dt;
+
+			if(Keyboard.GetState().IsKeyDown(Keys.Space))
+			{
+				if(sfxInstance.State != SoundState.Playing)
+					sfxInstance.Play();
+			}
+
+
+			base.Update(gameTime);
+		}
+
+		/// <summary>
+		/// This is called when the game should draw itself.
+		/// </summary>
+		/// <param name="gameTime">Provides a snapshot of timing values.</param>
+		protected override void Draw(GameTime gameTime)
+		{
+			GraphicsDevice.Clear(Color.CornflowerBlue);
+
+			spriteBatch.Begin();
+			spriteBatch.Draw(boxman, boxmanPos, Color.White);
+			spriteBatch.End();
+
+			base.Draw(gameTime);
+		}
+	}
+}
