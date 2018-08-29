3c22421213860734263bcce732d8305394555600
Magellanic <magellanicdev@gmail.com>
2018-08-29 19:17:13 +0100

    Completed guide for 2d cam

--- build.bat
+++ build.bat
@@ -3,4 +3,6 @@ start SpeedyHtmlBuilder.exe monogameTuts.shb
 start SpeedyHtmlBuilder.exe monogameTuts1.shb
 start SpeedyHtmlBuilder.exe advancedInput.shb
 start SpeedyHtmlBuilder.exe ControlsSource.shb
+start SpeedyHtmlBuilder.exe basic2dCamera.shb
+start SpeedyHtmlBuilder.exe 2dcameraSource.shb
 start SpeedyHtmlBuilder.exe About.shb
